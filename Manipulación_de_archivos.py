54613854e8b1bb6cf9ca4fa0b1558b49b60c59ab
valuschvartz <valentinaschvartz@gmail.com>
2023-04-05 17:14:42 -0300

    Guia Manipulacion Archivos

--- Manipulación_de_archivos.py
+++ Manipulación_de_archivos.py
@@ -34,7 +34,13 @@ def guardar_lineas(archivo, n):
 #  **Ejercicio 4**
 # Hacé un programa que lea un archivo, cuente la cantidad de palabras 
 # del archivo y luego imprima el resultado.
-
+def cantidad_palabras(archivo):
+    with open (archivo, "r") as file:
+        contador = 0
+        lineas = file.readlines()
+        for linea in lineas:
+            palabras = linea.split()
+            contador = contador + len(palabras)
 #  **Ejercicio 5**
 # Escribí un programa que lea un archivo, 
 # reemplace una letra por esa misma letra más un salto de línea y lo guarde en otro archivo.
@@ -42,14 +48,41 @@ def guardar_lineas(archivo, n):
 #  **Ejercicio 6**
 # Realizá un programa que lea un archivo, 
 # elimine todos los saltos de línea y lo guarde en otro archivo.
+def eliminar_saltos_de_linea(archivo_entrada, archivo_salida):
+    with open(archivo_entrada, 'r') as file_1:
+        with open(archivo_salida, 'w') as file_2:
+            for linea in file_1:
+                linea_sin_salto = linea.rstrip('\n')
+                file_2.write(linea_sin_salto)
+
 
 #  **Ejercicio 7**
 # Escribí un porgrama que lea un archivo e identifique 
 # la palabra más larga, la cual debe imprimir y decir cuantos caracteres tiene.
 
+
+def longest_word(archivo):
+    max_long = 0
+    palabra = ""
+    with open(archivo,"r") as f:
+        word_list = f.read().split()
+        for word in word_list:
+            if len(word) > max_long:
+                max_long = len(word)
+                palabra = word
+    print("La palabra más larga es", palabra, "con", max_long, "caracteres")
+longest_word("archivo1.txt")
+
+
 #  **Ejercicio 8**
 # Escribí un programa que abra dos documentos y guarde 
 # el contenido de ambos en un otro documento ya existente.
+
+def join_files(file1,file2,file3):
+    with open(file1,"r") as f1, open(file2, "r") as f2, open(file3,"a") as f3:
+        f3.write(f1.read()+f2.read())
+join_files("archivo1.txt","archivo2.txt","archivos_unidos.txt")
+
 
 #  **Ejercicio 9**
 # Realizá un programa que lea un archivo y obtenga 
@@ -61,3 +94,4 @@ def guardar_lineas(archivo, n):
 # # Escribí un programa que lea todos los archivos .txt de 
 # una carpeta dada (**Carpeta1**) y los añada a un archivo dentro de 
 # la carpeta _Resultado_, que a su vez se tiene que encontrar dentro de **Carpeta1**.
+
