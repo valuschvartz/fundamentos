d1a6635c9bd705cfb68ff1225f90b1f6b588ec3b
valuschvartz <valentinaschvartz@gmail.com>
2023-04-15 16:39:20 -0300

    POO

--- POO/Teoria/materiales/main.py
+++ POO/Teoria/materiales/main.py
@@ -1,1 +1,12 @@
-from aves import pepita
+from aves import pepita, anastasia, roberta
+
+print(pepita.volar_en_circulos())
+print(pepita.esta_feliz())
+print(pepita.energia)
+
+print(anastasia.energia)
+print(anastasia.esta_feliz())
+
+
+print(roberta.energia)
+print(roberta.esta_feliz())
